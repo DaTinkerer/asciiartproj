c4f03140daed9009531e8f72042eaaac3aa095cb
bryant <bryant>
2022-04-26 01:30:38 -0400

    added boostrap and some custom sass

--- app.py
+++ app.py
@@ -5,12 +5,17 @@ from flask import request
 from flask_cors import CORS
 from flask_wtf.csrf import CSRFProtect
 from decouple import config
+from flask_assets import Environment, Bundle
+from scss import Compiler
 app = Flask(__name__)
 app.secret_key = config('SECRET_KEY')
-WTF_CSRF_TIME_LIMIT = None
+app.config['WTF_CSRF_TIME_LIMIT'] = 604800
 csrf = CSRFProtect(app)
 CORS(app)
+assets = Environment(app)
 
+scss = Bundle('scss/main.scss', filters='pyscss', output='css/style.css')
+assets.register('scss_all', scss)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
